0539e1f48e48fe0c98de1b9f695c9c0c2106ed14
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-18 12:18:17 +0000

    zaklad tridy Dispatcher

---
+++ core/dispatcher.php
@@ -0,0 +1,39 @@
+<?php
+require_once ('View.php');
+require_once ('dispatcherinterface.php');
+
+/**
+ * @author gambler
+ * @version 1.0
+ * @created 18-V-2010 14:14:22
+ */
+class Dispatcher implements DispatcherInterface
+{
+
+	public $m_View;
+	public $m_ModulDispatcher;
+
+	public function __construct()
+	{
+	   //TODO: create instance of view
+	}
+
+
+
+	/**
+	 * Metoda se postara o zavolani spravneho controlleru
+	 * @param request
+	 * @return void	 
+	 */
+	public function dispatch(Request $request)
+	{
+	   //TODO: create instance of module dispatcher if modul exists
+	   echo "Dispatcher zavolan! <br>\n";
+	   
+	   echo "Modul: " . $request->get_module() . "<br>\n";
+	   echo "Controller" . $request->get_controler() . "<br>\n";
+	   echo "Action: " . $request->get_action() . "<br>\n";
+	}
+
+}
+?>
