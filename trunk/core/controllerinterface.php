30394ce482353d0ba744b1abd54b74d18707970e
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-21 23:21:39 +0000

    pridani ControllerInterface do core

---
+++ trunk/core/controllerinterface.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/**
+ * @author Jakub Holý
+ * @version 1.0
+ * @created 22-V-2010 1:14:13
+ */
+interface ControllerInterface
+{
+
+	/**
+	 * Zaregistruje instanci tridy request pro pouziti v controlleru
+	 * @param Request $req
+	 * @return void
+	 */
+	public function registerRequest(Request $req);
+
+	/**
+	 * Zaregistruje instanci tridy view pro pouziti v controlleru
+	 * @param View $view
+	 * @return void
+	 */
+	public function registerView(View $view);
+
+}
+?>
