3ef27a7154e6519aa1ae5c1f744f1bad6787cf63
wheder <wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-22 13:51:36 +0000

    Oprava viditelnosti konfigurace modulu user + oprava regexpu pro nick

--- trunk/modules/user/config/user_conf.php
+++ trunk/modules/user/config/user_conf.php
@@ -1,8 +1,12 @@
 <?php
+
+//global $user_config;
+
+$user_config = array();
 $user_config['nick']['maxlength'] = 30;
 $user_config['nick']['minlength'] = 3;
 $user_config['nick']['regexp']['match_required'] = true;
-$user_config['nick']['regexp']['content'] = '/[a-bA-B0-9]*/';
+$user_config['nick']['regexp']['content'] = '/[a-zA-Z0-9]*/';
 
 $user_config['password']['minlength'] = 4;
 $user_config['password']['maxlength'] = 30;
@@ -13,4 +17,5 @@ $user_config['mail']['minlength'] = 4;
 $user_config['mail']['maxlength'] = 30;
 $user_config['mail']['regexp']['match_required'] = true;
 $user_config['mail']['regexp']['content'] = '/^[^@]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$/';
+
 ?>
