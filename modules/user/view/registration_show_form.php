706eef6dd634209d6be996b698927762799b955e
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-22 12:21:55 +0000

    registracni formular

--- modules/user/view/registration_show_form.php
+++ modules/user/view/registration_show_form.php
@@ -1,1 +1,9 @@
-<p>tady mozna jednou bude registracni formular</p>
+<form action="/user/registration/register" method="post">
+<p>
+    Nick: <input type="text" name="nick" /><br />
+    Heslo: <input type="password" name="pass1" /><br />
+    Heslo znovu: <input type="password" name="pass2" /><br />
+    E-mail: <input type="text" name="mail" /><br />
+    <input type="submit" value="Registrovat"/>
+</p>
+</form>
