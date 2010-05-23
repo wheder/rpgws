3f5154eb2aa68413a53b576c7263fbfceb8f25d1
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-23 21:13:11 +0000

    uprava modelu pro detaily o uzivateli + info controller

---
+++ modules/user/info/controller.php
@@ -0,0 +1,72 @@
+<?php
+require_once ('..\..\..\core\controllerinterface.php');
+require_once ('User_Dispatcher.php');
+require_once ('..\model.php');
+
+/**
+ * @author Michal Hyn�ica
+ * @version 1.0
+ * @created 22-V-2010 12:31:28
+ */
+class User_Info_Controller implements ControllerInterface
+{
+
+	private $m_View;
+	private $m_Request;
+    private $config;
+    private $auth;
+    
+	function __construct()
+	{
+	    include dirname(__FILE__) . "/../config/user_conf.php"; 
+        $this->config = $user_config;
+        $auth = new Authentificator();
+	}
+
+
+
+	public function edit_action()
+	{
+	    
+	}
+
+	public function edit_form_action()
+	{
+	    $this->m_View->err = false;
+	    $user_id = $this->auth->logged_user();
+	    if($user_id < 1) {
+	        $this->m_View->err = true;
+	        $this->m_View->msg = "Nejste přihlášen.";
+	    } else {
+	        $user = new User_Model();
+	        $user->load($user_id);
+	        $this->m_View->user = $user;
+	        $this->m_View->fields = $user->get_detail_types();
+	    }
+	    $this->m_View->printPage();
+	}
+
+	/**
+	 * 
+	 * @param req
+	 */
+	public function registerRequest(Request $req)
+	{
+	    $this->m_Request = $req;
+	}
+
+	/**
+	 * 
+	 * @param view
+	 */
+	public function registerView(View $view)
+	{
+	    $this->m_View = $view;
+	}
+
+	public function show_info_action()
+	{
+	}
+
+}
+?>
---
+++ modules/user/view/info_edit_form.php
@@ -0,0 +1,42 @@
+<?php
+if($this->err)
+{
+?>
+<h3>Chyba:</h3>
+<p><?php echo $this->msg; ?></p>
+<?php 
+} else {
+?>
+
+<form action="user/info/edit" method="post">
+<p>
+    Nick: <input type="text" name="nick" maxlength="<?php echo $this->nick_max; ?>" value="<?php echo $this->user->nick;?>"/><br />
+    <br />
+    Změna hesla:<br />
+    Staré heslo: <input type="password" name="oldpass" maxlength="<?php echo $this->pass_max; ?>"/><br />
+    Nové heslo: <input type="password" name="newpass" maxlength="<?php echo $this->pass_max ?>"/><br />
+    Nové heslo (znovu pro kontrolu): <input type="password" name="newpass2" maxlength="<?php echo $this->pass_max ?>"/><br />
+    <br />
+    Nepovinné údaje:<br />
+    <table>
+    <tr>
+    	<th>Název</th>
+    	<th>Hodnota</th>
+    	<th>Veřejný</th>
+    </tr>
+   <?php foreach($this->field as $field) {
+       echo "    <tr>\n";
+       echo "        <td>$field:</td>\n";
+       echo "        <td><input type=\"text\" name=\"$field\" value=\"" . $this->user->get_detail($field) . "\" /></td>";
+       echo "        <td><input type=\"checkbox\" name=\"public[$field]\"";
+       if($this->user->is_public($field)) echo " checked=\"checked\"";
+       echo " value=\"1\"></td>\n";
+       echo "    </tr>\n"; 
+   }?>
+   </table>
+   <br />
+   <input type="submit" value="Odeslat" />
+</p>
+</form>
+
+<?php }?>
