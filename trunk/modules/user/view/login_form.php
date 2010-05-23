2236e6ec3c8ad92af16e4d62303a3ddd56a8d158
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-23 01:23:12 +0000

    oprava formulare na prihlaseni

--- trunk/modules/user/view/login_form.php
+++ trunk/modules/user/view/login_form.php
@@ -1,7 +1,7 @@
 <form action="/user/login/login" method="post">
 <p>
     Nick: <input type="text" name="nick" maxlength="<?php echo $this->nick_max ?>" /><br />
-    Heslo: <input type="text" name="pass" maxlength="<?php echo $this->pass_max ?>"/><br />
+    Heslo: <input type="password" name="pass" maxlength="<?php echo $this->pass_max ?>"/><br />
     <input type="submit" value="Přihlásit"/>
 </p>
 </form>
