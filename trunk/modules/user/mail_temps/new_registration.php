67dda3484a12077de6169c6b197bd7eeb70fff96
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-23 00:39:46 +0000

    uprava obsahu mailu

--- trunk/modules/user/mail_temps/new_registration.php
+++ trunk/modules/user/mail_temps/new_registration.php
@@ -1,6 +1,6 @@
 Vas ucet na http://www.wheder.info byl uspesne vytvoren.
 
 Vase udaje:
-Nick: <?php echo $vars['nick']; ?>
-E-mail: <?php echo $vars['mail']; ?>
+Nick: <?php echo $vars['nick'] . "\r\n"; ?>
+E-mail: <?php echo $vars['mail'] . "\r\n"; ?>
 Heslo: <?php echo $vars['pass']; ?>
