07dc866df09b37c11c72afa4187cb0ae5267ee90
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-23 23:21:27 +0000

    datum narozeni v registracnim formulari

--- modules/user/view/registration_show_form.php
+++ modules/user/view/registration_show_form.php
@@ -2,6 +2,25 @@
 <p>
     Nick: <input type="text" name="nick" maxlength="<?php echo $this->nick_max ?>" /><br />
     E-mail: <input type="text" name="mail" maxlength="<?php echo $this->mail_max ?>"/><br />
+    <br />
+    Datum narozeni:<br />
+    Den: <select name="day">
+        <?php for($i = 1; $i < 32; $i++) { ?>
+             <option value="<?php echo $i; ?>"><?php echo $i;?></option>    
+         <?php   }   ?>
+        </select>
+    Měsíc: <select name="month">
+    	<?php for($i = 1; $i < 13; $i++) { ?>
+             <option value="<?php echo $i; ?>"><?php echo $i;?></option>    
+         <?php   }   ?>
+    </select>
+    Rok: <select name="year">
+    	<?php
+    	   $year_end = date("Y", now());
+    	   for($i = 1930; $i < 13; $i++) {	?>
+    	   <option value="<?php echo $i; ?>"><?php echo $i;?></option>
+    	<?php } ?>
+    </select><br />
     <input type="submit" value="Registrovat"/>
 </p>
 </form>
