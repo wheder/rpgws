90bbaae184177c03e93ce6a703356fa66264fce0
wheder <wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-26 21:38:40 +0000

    setup guide

---
+++ trunk/config/global.php
@@ -0,0 +1,24 @@
+<?php
+
+$rpgws_config = Array();
+$rpgws_config['db'] = Array();
+$rpgws_config['db']['adapter'] = 'mysqlpdo'; //required
+$rpgws_config['db']['host'] = 'localhost';
+$rpgws_config['db']['name'] = 'rpgws';
+$rpgws_config['db']['user'] = 'rpgws';
+$rpgws_config['db']['pass'] = 'secret password';
+$rpgws_config['db']['port'] = '3306';
+$rpgws_config['db']['prefix'] = 'RPGWS_';
+
+//better keep it default
+$rpgws_config['logs']['level'] = 2;
+
+$rpgws_config['layout']['default'] = 'default.php';
+$rpgws_config['view']['error'] = 'error.php';
+
+$rpgws_config['flood_prot']['time_limit'] = 5*60; //limit in seconds
+$rpgws_config['flood_prot']['limit'] = 5; //amount of allowed attempts
+//$rpgws_config['db']['socket'] = '';
+
+
+?>
--- trunk/config/setup.php
+++ trunk/config/setup.php
@@ -5,6 +5,14 @@
   <title>RPG Web System - Install</title>
 </head>
 <body>
-  create <?php echo RPGWS_CONFIG; ?>/local.php
+  <h1>Congratulations!</h1>
+  <p>You have successfully installed the RPG Web System!</p>
+<p>Yet, a few steps are neeed to run your own project.</p>
+<ol>
+  <li>Copy the file '<?php echo RPGWS_CONFIG; ?>/global.php' to '<?php echo RPGWS_CONFIG; ?>/local.php'</li>
+  <li>Edit the file '<?php echo RPGWS_CONFIG; ?>/local.php'</li>
+  <li>Set the apropriate values of variables.</li>
+  <li>Done</li>
+  </ol>
 </body>
 </html>
