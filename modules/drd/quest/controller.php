c6bb2b7cf52667b4004562ff3b4a6f0a09e23a32
Wheder <Wheder@090d9c42-aefe-0998-e1cd-aee8488cd921>
2010-05-27 00:44:14 +0000

    slozka pro view skripty modulu DrD

--- modules/drd/quest/controller.php
+++ modules/drd/quest/controller.php
@@ -1,58 +1,63 @@
 <?php
-require_once ('DrD_Dispatcher.php');
-require_once ('..\..\..\core\controllerinterface.php');
 
 /**
- * @author Michal Hynèica
+ * @author Jakub HolÃ½
  * @version 1.0
  * @created 25-V-2010 16:46:15
  */
 class DrD_Quest_Controller implements ControllerInterface
 {
 
-	public $m_DrD_Dispatcher;
+    private $m_View;
+    private $m_Request;
 
-	function __construct()
-	{
-	}
+    function __construct()
+    {
+    }
 
-	function __destruct()
-	{
-	}
+    function __destruct()
+    {
+    }
 
 
 
-	public function add_post_action()
-	{
-	}
+    public function add_post_action()
+    {
+    }
 
-	public function create_action()
-	{
-	}
+    public function create_action()
+    {
+    }
 
-	public function create_form_action()
-	{
-	}
+    public function create_form_action()
+    {
+    }
 
-	/**
-	 * 
-	 * @param req
-	 */
-	public function registerRequest(Request $req)
-	{
-	}
+    /**
+     * 
+     * @param req
+     */
+    public function registerRequest(Request $req)
+    {
+        $this->m_Request = $req;
+    }
 
-	/**
-	 * 
-	 * @param view
-	 */
-	public function registerView(View $view)
-	{
-	}
+    /**
+     * Metoda nastavi pouzivane View
+     * @param view
+     */
+    public function registerView(View $view)
+    {
+        $this->m_View = $view;
+    }
 
-	public function view_action()
-	{
-	}
+    public function view_action()
+    {
+    }
 
+    public function index_action() 
+    {
+        header('location: /drd/quest/view');  
+    }
 }
 ?>
